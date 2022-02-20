4966c324793843345f716a5dc37f836588a19e08
住吉翼 <tsubasa.t1119@gmail.com>
2022-02-20 12:20:53 +0900

    トランザクションの追加

--- blockchain.py
+++ blockchain.py
@@ -34,20 +34,46 @@ class BlockChain(object):
         sorted_block = json.dumps(block, sort_keys=True)
         return hashlib.sha256(sorted_block.encode()).hexdigest()
 
+    def add_transaction(self,
+                        sender_blockchain_address,
+                        recipient_blockchain_address,
+                        value):
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': sender_blockchain_address,
+            'recipient_blockchain_adress': recipient_blockchain_address,
+            'value': float(value)
+        })
+        self.transaction_pool.append(transaction)
+        return True
+
 def pprint(chains):
     for i, chain in enumerate(chains):
         print(f'{"="*25} Chain {i} {"="*25}')
         for k, v in chain.items():
-            print(f'{k:15}{v}')
+            if k == 'transactions':
+                print(k)
+                for d in v:
+                    print(f'{"-"*40}')
+                    for kk, vv in d.items():
+                        print(f'{kk:30}{vv}')
+            else:
+                print(f'{k:15}{v}')
     print(f'{"*"*25}')
 
 if __name__ == '__main__':
     block_chain = BlockChain()
     pprint(block_chain.chain)
+
+    # トランザクションの追加
+    block_chain.add_transaction('A', 'B', 1.0)
     # 一個前のブロックを使ったハッシュ値
     previous_hash = block_chain.hash(block_chain.chain[-1])
     block_chain.create_block(2, previous_hash)
     pprint(block_chain.chain)
+
+    # トランザクションの追加
+    block_chain.add_transaction('C', 'D', 2.0)
+    block_chain.add_transaction('X', 'Y', 5.0)
     # 一個前のブロックを使ったハッシュ値
     previous_hash = block_chain.hash(block_chain.chain[-1])
     block_chain.create_block(3, previous_hash)
