a8ba613b0a4716802332a4e7546b31b03469198b
住吉翼 <tsubasa.t1119@gmail.com>
2022-02-20 11:39:46 +0900

    ブロックのハッシュ値を作成

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
+import hashlib
+import json
 import logging
 import sys
 import time
+
+import utils
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -9,22 +13,26 @@ class BlockChain(object):
     def __init__(self):
         self.transaction_pool = []
         self.chain = []
-        self.create_block(0, 'init hash')
+        self.create_block(0, self.hash({}))
 
     def create_block(self, nonce, previous_hash):
         # ブロックの中身
-        block = {
+        block = utils.sorted_dict_by_key({
             'timestamps': time.time(),
             # トランザクションプールのトランザクションを挿入
             'transactions': self.transaction_pool,
             'nonce': nonce,
             'previous_hash': previous_hash
-        }
+        })
         # ブロックをブロックチェーンに追加
         self.chain.append(block)
         # トランザクションプール内のトランザクションは移行できたため中身をクリア
         self.transaction_pool = []
         return block
+
+    def hash(self, block):
+        sorted_block = json.dumps(block, sort_keys=True)
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
 
 def pprint(chains):
     for i, chain in enumerate(chains):
@@ -36,7 +44,11 @@ def pprint(chains):
 if __name__ == '__main__':
     block_chain = BlockChain()
     pprint(block_chain.chain)
-    block_chain.create_block(2, 'hash1')
+    # 一個前のブロックを使ったハッシュ値
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(2, previous_hash)
     pprint(block_chain.chain)
-    block_chain.create_block(3, 'hash2')
+    # 一個前のブロックを使ったハッシュ値
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(3, previous_hash)
     pprint(block_chain.chain)
